26151222b90269410f29017e05d5d6c9eb02cf64
fatmaddurann <96945947+fatmaddurann@users.noreply.github.com>
2025-05-07 21:54:29 +0300

    Update main.py

--- frontend/backend/main.py
+++ frontend/backend/main.py
@@ -1,5 +1,14 @@
+import os
+import base64
+
+key_b64 = os.getenv("GCP_SERVICE_ACCOUNT_KEY")
+if key_b64:
+    with open("/tmp/service-account.json", "wb") as f:
+        f.write(base64.b64decode(key_b64))
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/tmp/service-account.json"
 from fastapi import FastAPI
 from routes import video_analysis, live_analysis
+
 
 app = FastAPI()
 
